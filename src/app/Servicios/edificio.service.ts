86f2306aa97bad19b03df80f598a18a73f2b806a
JulioAI2003 <102555436+JulioAI2003@users.noreply.github.com>
2022-04-02 23:12:35 -0500

    asd

--- src/app/Servicios/edificio.service.ts
+++ src/app/Servicios/edificio.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from "@angular/common/http";
 import{Observable} from 'rxjs';
 import { of } from 'rxjs';
 import { Edificio } from '../Edificios/edificio';
-import { EDIFICIOS } from '../Edificios/edificios.json';
 @Injectable({
   providedIn: 'root'
 })
