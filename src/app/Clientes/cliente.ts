3d187f5f5c8526658953e0324bbc2988e6b3bb1d
JulioAI2003 <102555436+JulioAI2003@users.noreply.github.com>
2022-04-23 01:41:53 -0500

    CLIENTE

--- src/app/Clientes/cliente.ts
+++ src/app/Clientes/cliente.ts
@@ -1,2 +1,11 @@
 export class Cliente {
+  id!: number;
+  nombres!: string;
+  a_paterno!: string;
+  a_materno!: string;
+  dni!: string;
+  telefono!: string;
+  correo!: string;
+  contrasena!: string;
 }
+
